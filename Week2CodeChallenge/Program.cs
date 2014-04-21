d3e357410b40da793386abd5047ee2c9a21647a9
LeeMontoya <jleemontoya@gmail.com>
2014-04-21 13:36:08 -0600

    finished

--- Week2CodeChallenge/Program.cs
+++ Week2CodeChallenge/Program.cs
@@ -86,36 +86,50 @@ namespace Week2CodeChallenge
         //declare function called TextStats with parameter called input
         static void TextStats(string input)
         {
-            //set variables to 0
-            int numChars = input.Length - 1;
-            int numWords = 0;
+            //create variables 
+            var numChar = input.Length;
+            var numCharSplit = input.Split(' ');
+            var numWords = numCharSplit.Length;
             int numVowels = 0;
-            int numCons = 0;
-            int numSpecial = 0;
-            int longest = 0;
-            int shortest = 0;
+            int numConsonant = 0;
+            int numSpecChar = 0;
 
-
-            //begin for loop to run through the string to count the given variables
-            for (var i = 0; i <= input.Length - 1; i++)
+            var biggestWord = "";
+            var shortestWord = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+            //start for loop to run through the string passed in to find exact numbers of information 
+            for (int i = 0; i < numCharSplit.Length; i++)
             {
 
-                var currentVowels = input[i];
-                //counting how many vowels in the loop
-                if (currentVowels == 'a' || currentVowels == 'e' || currentVowels == 'i' || currentVowels == 'o' || currentVowels == 'u')
+                if (numCharSplit[i].Length > biggestWord.Length)
                 {
-                    numVowels += 1;
+                    biggestWord = numCharSplit[i];
                 }
-             
             }
 
-            Console.WriteLine("Number of characters: " + numChars);
-            Console.WriteLine("Number of words: " + numWords);
-            Console.WriteLine("Number of vowels: " + numVowels);
-            Console.WriteLine("Number of consonants: " + numCons);
-            Console.WriteLine("Number of special characters: " + numSpecial);
-            Console.WriteLine("Longest word: " + longest);
-            Console.WriteLine("Shortest word: " + shortest);
+            for (int i = 0; i < numCharSplit.Length; i++)
+            {
+                if (numCharSplit[i].Length < shortestWord.Length)
+                {
+                    shortestWord = numCharSplit[i];
+                }
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i].ToString() == "a" || input[i].ToString() == "e" || input[i].ToString() == "i" || input[i].ToString() == "o" || input[i].ToString() == "u")
+                {
+                    numVowels = numVowels + 1;
+                }
+                else if (input[i].ToString() == "." || input[i].ToString() == "," || input[i].ToString() == "?" || input[i].ToString() == "!" || input[i].ToString() == "'")
+                {
+                    numSpecChar = numSpecChar + 1;
+                }
+
+                else if (input[i].ToString() != " ")
+                {
+                    numConsonant = numConsonant + 1;
+                }
+            }
         }
         
     }
